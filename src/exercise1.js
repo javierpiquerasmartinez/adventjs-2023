8dc6fef9875e74e1a50b9ffecbf2e34c84d6e8cb
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-12-15 08:15:53 +0100

    Exercise 1

---
+++ src/exercise1.js
@@ -0,0 +1,21 @@
+/* 
+En la fábrica de juguetes del Polo Norte, cada juguete tiene un número de identificación único.
+
+Sin embargo, debido a un error en la máquina de juguetes, algunos números se han asignado a más de un juguete.
+
+¡Encuentra el primer número de identificación que se ha repetido, donde la segunda ocurrencia tenga el índice más pequeño!
+
+En otras palabras, si hay más de un número repetido, debes devolver el número cuya segunda ocurrencia aparezca primero en la lista. 
+Si no hay números repetidos, devuelve -1.
+*/
+
+function findFirstRepeated(gifts) {
+    const processed = []
+
+    for(let gift of gifts){
+        if(processed.includes(gift)) return gift
+        processed.push(gift)
+    }
+
+    return -1
+}
