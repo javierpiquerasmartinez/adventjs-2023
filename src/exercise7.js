0edd57c19113dff351bfda45ff0d33e98f2bcc03
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-12-19 08:27:57 +0100

    Description exercise 7

---
+++ src/exercise7.js
@@ -0,0 +1,43 @@
+/* 
+Santa está experimentando con nuevos diseños de regalos y necesita tu ayuda para visualizarlos en 3D.
+
+Tu tarea es escribir una función que, dado un tamaño n (entero), 
+genere un dibujo de un regalo en 3D utilizando caracteres ASCII.
+
+Las líneas de los regalos se dibujan con # y las caras con el símbolo que nos pasan como parámetro:
+*/
+
+function drawGift(size, symbol) {
+    // Code here
+    return '#\n'
+}
+
+drawGift(4, '+')
+
+/*
+   ####
+  #++##
+ #++#+#
+####++#
+#++#+#
+#++##
+####
+*/
+
+drawGift(5, '*')
+/*
+    #####
+   #***##
+  #***#*#
+ #***#**#
+#####***#
+#***#**#
+#***#*#
+#***##
+#####
+*/
+
+drawGift(1, '^')
+/*
+#
+*/
