43c8c390f496138203fe04130c9049dad5885eab
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-12-19 07:56:05 +0100

    tabs

--- src/exercise5.js
+++ src/exercise5.js
@@ -36,14 +36,14 @@ function cyberReindeer(road, time) {
     let currentStep = '.'
 
     for(let i = 1; i < time; i++) {
-        if(i === 5) road = road.replaceAll('|', '*')
+      if(i === 5) road = road.replaceAll('|', '*')
 
-        const canMove = road.match(/S[\*\.]/g)
-        if(canMove) {
-        road = road.replace(canMove[0], currentStep + 'S')
-        currentStep = canMove[0][1]
-        }
-        snapshots.push(road)
+      const canMove = road.match(/S[\*\.]/g)
+      if(canMove) {
+      road = road.replace(canMove[0], currentStep + 'S')
+      currentStep = canMove[0][1]
+      }
+      snapshots.push(road)
     }
     return snapshots
 }
