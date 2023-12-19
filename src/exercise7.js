5aaea9e98c644f0ad1eb98b8eb83d637a074be0b
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-12-19 17:05:20 +0100

    Exercise 7

--- src/exercise7.js
+++ src/exercise7.js
@@ -7,12 +7,27 @@ genere un dibujo de un regalo en 3D utilizando caracteres ASCII.
 Las líneas de los regalos se dibujan con # y las caras con el símbolo que nos pasan como parámetro:
 */
 
-/* function drawGift(size, symbol) {
-  // Code here
-  return '#\n'
+function drawGift(size, symbol) {
+  if(size === 1) return '#\n'
+  const rows = []
+
+  //We set the special rows
+  rows[0] = '#'.repeat(size).padStart(size * 2 - 1)
+  rows[size - 1] = '#'.repeat(size).concat(`${symbol.repeat(size - 2)}#`)
+  rows[size * 2 - 2] = '#'.repeat(size)
+  
+  //We follow the pattern
+  for(let i = 1; i < size - 1; i++) {
+    rows[i] = `#${symbol.repeat(size - 2)}#${symbol.repeat(i - 1)}#`
+      .padStart(size * 2 - 1)
+    rows[size * 2 - 2 - i] = 
+      `#${symbol.repeat(size - 2)}#${symbol.repeat(i - 1)}#`
+  }
+
+  return rows.join('\n').concat('\n')
 }
 
-drawGift(4, '+') */
+//drawGift(4, '+')
 
 /*
    ####
@@ -24,7 +39,7 @@ drawGift(4, '+') */
 ####
 */
 
-// drawGift(5, '*')/*  */
+console.log(drawGift(10, '^'))
 /*
     #####
    #***##
